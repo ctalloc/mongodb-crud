7d9008ca4b13d72af11ddb8b054589db4e4aa062
ctalloc <cecilia.tallo@gmail.com>
2021-04-22 16:12:29 +0200

    :pencil:Implement db render with template engine.

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const { Db } = require('mongodb');
 
 MongoClient.connect('mongodb+srv://grogu:pa55w0rd@cluster0.w04ud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useUnifiedTopology: true
 }, (err, client) => {
     if (err) return console.error(err);
     console.log('Connected to db');
-    const db = client.db('sw-quotes')
+
+    const db = client.db('sw-quotes');
+
+    const quotesCollection = db.collection('quotes');
+
+    app.set('view engine', 'ejs')
+
     app.listen(3000, () => {
         console.log('listening on 3000');
     });
@@ -18,11 +25,19 @@ MongoClient.connect('mongodb+srv://grogu:pa55w0rd@cluster0.w04ud.mongodb.net/myF
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.get('/', (req, res) => {
-        res.sendFile('/Users/oni/Repositorios/mongodb-crud/' + 'index.html')
+        db.collection('quotes').find().toArray()
+            .then(results => {
+                res.render('index.ejs', { quotes: results})
+            })
+            .catch(error => console.error(error));
     })
 
     app.post('/quotes', (req, res) => {
-        console.log(req.body);
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
     })
 });
 
