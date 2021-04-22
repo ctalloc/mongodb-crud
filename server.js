05d98ee325efa783162e225ada77266305cada36
ctalloc <cecilia.tallo@gmail.com>
2021-04-22 15:37:02 +0200

    :card_file_box: Sucessfully connect to Mongodb cluster.

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ console.log("May Node be with you");
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(3000, () => {
-    console.log('listening on 3000');
+MongoClient.connect('mongodb+srv://grogu:pa55w0rd@cluster0.w04ud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) return console.error(err);
+    console.log('Connected to db');
+    const db = client.db('sw-quotes')
+    app.listen(3000, () => {
+        console.log('listening on 3000');
+    });
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/', (req, res) => {
+        res.sendFile('/Users/oni/Repositorios/mongodb-crud/' + 'index.html')
+    })
+
+    app.post('/quotes', (req, res) => {
+        console.log(req.body);
+    })
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile('/Users/oni/Repositorios/mern-crud/' + 'index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body);
-})
